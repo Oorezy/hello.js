5086515672f58a7cb127e38df32444593ca02fcc
Oorezy <134717612+Oorezy@users.noreply.github.com>
2026-01-23 13:07:48 +0000

    exercise

--- test.js
+++ test.js
@@ -4,3 +4,18 @@ const makeAdder = (x) => {
 }
 const incr = makeAdder(1); 
 console.log(incr(2)); 
+
+//Exercise
+const addSum = (f,m) =>{
+  let sum = 0;
+  f.array.forEach(f1 => {
+    m.array.forEach(m1 =>{
+      if(m1 % f1 === 0){
+        sum += m1;
+      }
+    });
+  });
+  return sum;
+}
+
+console.log("This is the sum: "+ addSum([3,5],[1,2,3,4,5,6,7,8,9]))
