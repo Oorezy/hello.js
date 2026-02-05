22cac51fe090e00cb0afb6ec833453c93f7d548e
Oorezy <134717612+Oorezy@users.noreply.github.com>
2026-02-05 12:18:17 +0000

    url GET function

--- test.js
+++ test.js
@@ -19,3 +19,11 @@ const addSum = (f,m) =>{
 }
 
 console.log("This is the sum for multiples: "+ addSum([3,5],[1,2,3,4,5,6,7,8,9]))
+let url = "https://pokeapi.co/api/v2/pokemon/ditto";
+async function fetchAndDisplay(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  document.getElementById("output").textContent =
+    JSON.stringify(data, null, 2);
+}
